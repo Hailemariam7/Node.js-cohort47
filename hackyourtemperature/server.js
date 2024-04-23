b70b5b8d261b587e1c4c4b33affbabbbac01005a
Hailemariam <htsigabu@gmail.com>
2024-04-23 16:46:10 +0200

    hailemariam-w2-node

--- hackyourtemperature/server.js
+++ hackyourtemperature/server.js
@@ -1,19 +1,7 @@
-import express from "express";
+import app from "./app.js";
 
-const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.json());
-
-app.get("/", (req, res) => {
-  res.send("Hello from backend to frontend!");
-});
-
-app.post("/weather", (req, res) => {
-  const { cityName } = req.body;
-  res.json({ cityName });
-});
-
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Listening on port: ${PORT}`);
 });
---
+++ hackyourtemperature/sources/keys.js
@@ -0,0 +1,4 @@
+const keys = {
+  API_KEY: "664a0f0cd4328f09056a0a70789762d7",
+};
+export default keys;
